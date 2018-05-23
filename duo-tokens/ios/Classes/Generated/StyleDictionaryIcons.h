d175df3ec07e269e6cc672eb994ec34362a8fe30
Ryan Williamson-Cardneau <ryan@ryans-macbook.lan>
2018-05-22 22:30:26 -0400

    trying to fix build script

---
+++ duo-tokens/ios/Classes/Generated/StyleDictionaryIcons.h
@@ -0,0 +1,15 @@
+
+//
+// StyleDictionaryIcons.h
+//
+// Do not edit directly, generated on Tue May 22 2018 21:47:51 GMT-0400 (EDT)
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+
+@interface StyleDictionaryIcons : NSObject
++ (NSArray *)values;
+@end
---
+++ duo-tokens/ios/Classes/Generated/StyleDictionarySize.m
@@ -0,0 +1,53 @@
+
+//
+// StyleDictionarySize.m
+//
+// Do not edit directly, generated on Tue May 22 2018 21:47:51 GMT-0400 (EDT)
+//
+
+#import "StyleDictionarySize.h"
+
+
+
+float const StyleDictionarySizeBase8 = 8.00f;
+float const StyleDictionarySizeBase10 = 10.00f;
+float const StyleDictionarySizeBase12 = 12.00f;
+float const StyleDictionarySizeBase14 = 14.00f;
+float const StyleDictionarySizeBase16 = 16.00f;
+float const StyleDictionarySizeBase20 = 20.00f;
+float const StyleDictionarySizeBase24 = 24.00f;
+float const StyleDictionarySizeBase32 = 32.00f;
+float const StyleDictionarySizeBase40 = 40.00f;
+float const StyleDictionarySizeBase48 = 48.00f;
+float const StyleDictionarySizeFontXxsmall = ;
+float const StyleDictionarySizeFontXsmall = ;
+float const StyleDictionarySizeFontSmall = NaNf;
+float const StyleDictionarySizeFontMedium = NaNf;
+float const StyleDictionarySizeFontLarge = NaNf;
+float const StyleDictionarySizeFontXlarge = NaNf;
+float const StyleDictionarySizeFontBase = NaNf;
+float const StyleDictionarySizeLineHeightSmall = ;
+float const StyleDictionarySizeLineHeightMedium = ;
+float const StyleDictionarySizeLineHeightLarge = ;
+float const StyleDictionarySizeLineHeightBase = 24.00f;
+float const StyleDictionarySizeWeightBase = 4800.00f;
+float const StyleDictionarySizeWeightMedium = 9600.00f;
+float const StyleDictionarySizeWeightBold = 14400.00f;
+float const StyleDictionarySizeIconXsmall = ;
+float const StyleDictionarySizeIconSmall = 10.00f;
+float const StyleDictionarySizeIconBase = 16.00f;
+float const StyleDictionarySizeIconLarge = 24.00f;
+float const StyleDictionarySizeIconXl = 32.00f;
+float const StyleDictionarySizeIconXxl = 40.00f;
+float const StyleDictionarySizeMarginXsmall = ;
+float const StyleDictionarySizeMarginSmall = 16.00f;
+float const StyleDictionarySizeMarginMedium = 24.00f;
+float const StyleDictionarySizeMarginLarge = 32.00f;
+float const StyleDictionarySizeMarginXlarge = 40.00f;
+float const StyleDictionarySizeMarginXxlarge = 48.00f;
+float const StyleDictionarySizePaddingXsmall = ;
+float const StyleDictionarySizePaddingSmall = 16.00f;
+float const StyleDictionarySizePaddingMedium = 24.00f;
+float const StyleDictionarySizePaddingLarge = 32.00f;
+float const StyleDictionarySizePaddingXlarge = 40.00f;
+float const StyleDictionarySizePaddingXxlarge = 48.00f;
